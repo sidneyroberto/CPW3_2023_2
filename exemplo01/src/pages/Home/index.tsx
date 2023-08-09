e421fdec91362d49eb8c3c91d880571d28db9f8a
Sidney Sousa <sidney.sousa@ifms.edu.br>
2023-08-08 21:37:31 -0400

    atualizando exemplo01

--- exemplo01/src/pages/Home/index.tsx
+++ exemplo01/src/pages/Home/index.tsx
@@ -1,3 +1,51 @@
-const Home = () => <h1>Home</h1>
+import { useEffect, useState } from 'react'
+
+const Home = () => {
+  // Área do state
+  const [number1, setNumber1] = useState(0)
+  const [number2, setNumber2] = useState(0)
+  const [result, setResult] = useState(0)
+  const [sentence, setSentence] = useState('')
+  const [numberOfChars, setNumberOfChars] = useState(0)
+
+  const sum = () => setResult(number1 + number2)
+
+  useEffect(() => {
+    const size = sentence.length
+    setNumberOfChars(size)
+  }, [sentence])
+
+  // Área de renderização
+  return (
+    <div>
+      <div>
+        <input
+          type='number'
+          id='number1'
+          value={number1}
+          onChange={(e) => setNumber1(Number(e.target.value))}
+        />
+        <span>+</span>
+        <input
+          type='number'
+          id='number2'
+          value={number2}
+          onChange={(e) => setNumber2(Number(e.target.value))}
+        />
+        <button onClick={() => sum()}>Somar</button>
+        <span>{result}</span>
+      </div>
+
+      <div>
+        <input
+          type='text'
+          value={sentence}
+          onChange={(e) => setSentence(e.target.value)}
+        />
+        <span>Quantidade de caracteres: {numberOfChars}</span>
+      </div>
+    </div>
+  )
+}
 
 export default Home
