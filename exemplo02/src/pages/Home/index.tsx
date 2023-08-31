a990c39f6a38be2a928ae554e73c0f3ede18d291
Sidney Sousa <sidney.sousa@ifms.edu.br>
2023-08-30 20:28:30 -0400

    iniciando a página home

--- exemplo02/src/pages/Home/index.tsx
+++ exemplo02/src/pages/Home/index.tsx
@@ -1,3 +1,16 @@
-const Home = () => <h1>Home</h1>
+import styles from './styles.module.css'
+
+const Home = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.searchArea}>
+        <input type='text' className={styles.searchInput} />
+        <button className={styles.searchButton}>Pesquisar</button>
+      </div>
+
+      <div className={styles.resultsArea}></div>
+    </div>
+  )
+}
 
 export default Home
